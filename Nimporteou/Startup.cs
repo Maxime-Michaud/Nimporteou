f7f9a7ecb9bcaf477c23bb0189accba77939362f
Maxime <maxime_m@live.ca>
2016-11-10 09:27:01 -0500

    okok

---
+++ Nimporteou/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nimporteou.Startup))]
+namespace Nimporteou
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
