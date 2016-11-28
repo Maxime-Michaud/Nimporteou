24afe72baf4de7bdcd00f83050862808d0dd1440
Nicholas <nickmarcil@hotmail.com>
2016-11-28 10:07:27 -0500

    Login marche pas

--- src/nimporteou/wwwroot/js/site.js
+++ src/nimporteou/wwwroot/js/site.js
@@ -1,1 +1,12 @@
 ﻿// Write your Javascript code.
+function loginModal(){
+    modal.style.display = "block";
+    $.ajax({
+        type: "POST",
+        url: 'http://localhost:4000/Account/login',
+        data: {},
+        success: function (html) {
+            document.getElementById("modal-content").innerHTML = html;
+        }
+    });
+}
