38ffc7012a85ad7784a04931096a3499a10be9c3
Keven <Keven@LAPTOP-2JB8D6D1>
2016-12-05 10:26:51 -0500

    map thing sa d'l'air

--- src/nimporteou/wwwroot/_references.js
+++ src/nimporteou/wwwroot/_references.js
@@ -1,5 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="js/maps.js" />
 /// <reference path="js/site.js" />
 /// <reference path="js/site.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
