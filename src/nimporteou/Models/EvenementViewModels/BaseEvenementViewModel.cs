5b233d159621e7528273510b556ab430d722115d
Maxime <maxime_m@live.ca>
2016-12-05 10:21:42 -0500

    mergev2

--- src/nimporteou/Models/EvenementViewModels/BaseEvenementViewModel.cs
+++ src/nimporteou/Models/EvenementViewModels/BaseEvenementViewModel.cs
@@ -1,16 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace nimporteou.Models.EvenementViewModels
 {
     public class BaseEvenementViewModel
     {
         public int EvenementID { get; set; }
-
+        public string Nom { get; set; }
+        public string CheminPhoto { get; set; }
+        public int? PrixBillet { get; set; }
         public string AdresseComplete { get; set; }
         public bool BilletNecessaire { get; set; }
-        public string CheminPhoto { get; set; }
         public DateTime Debut { get; set; }
-        public string Nom { get; set; }
-        public int? PrixBillet { get; set; }
     }
 }
--- src/nimporteou/Models/EvenementViewModels/EvenementViewModel.cs
+++ src/nimporteou/Models/EvenementViewModels/EvenementViewModel.cs
@@ -14,10 +14,6 @@ namespace nimporteou.Models.EvenementViewModels
         [StringLength(1000, ErrorMessage = "Le nom doit contenir moins de 1000 caratères")]
         public string Description { get; set; }
 
-        [StringLength(100, ErrorMessage = "Le nom doit contenir plus de 5 caratères et moins de 100 caratères", MinimumLength = 5)]
-        public string Nom { get; set; }
-        public string CheminPhoto { get; set; }
-        public int? PrixBillet { get; set; }
         public string Categorie { get; set; }
     }
 }
