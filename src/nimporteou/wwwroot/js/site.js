cbfd8cbfff6dfa40dc2f7b271c530e06fb164c8e
Nicholas <nickmarcil@hotmail.com>
2016-12-08 13:07:40 -0500

    fenetre modal pour login

--- src/nimporteou/wwwroot/js/site.js
+++ src/nimporteou/wwwroot/js/site.js
@@ -1,11 +1,31 @@
 ﻿// Write your Javascript code.
-function loginModal(){
-    modal.style.display = "block";
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+      
+function modifierService2() {
+
     $.ajax({
-        type: "POST",
-        url: 'http://localhost:4000/Account/login',
+        type: "GET",
+        url: '/Account/login',
         data: {},
         success: function (html) {
+            modal.style.display = "block";
             document.getElementById("modal-content").innerHTML = html;
         }
     });
