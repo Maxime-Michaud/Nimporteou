a3003c4b3221108be0e349060fc11ae33ee5196c
Keven <Keven@LAPTOP-2JB8D6D1>
2016-12-12 10:37:40 -0500

    le meme truc la

--- src/nimporteou/wwwroot/_references.js
+++ src/nimporteou/wwwroot/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/maps.js" />
 /// <reference path="js/site.js" />
 /// <reference path="js/site.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
